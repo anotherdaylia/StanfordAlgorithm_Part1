92072d7ae57d3c2ba8516a380c35ee0cc0f2a89f
Lia <anotherdaylia@gmail.com>
2015-09-24 13:37:09 -0700

    update the scc.java and vertex.java

--- StanfordAlgorithm/src/SCC.java
+++ StanfordAlgorithm/src/SCC.java
@@ -1,27 +1,3 @@
-/**
- * The file contains the edges of a directed graph. Vertices are labeled as positive integers from 1 to 875714. 
- * Every row indicates an edge, the vertex label in first column is the tail and the vertex label 
- * in second column is the head (recall the graph is directed, and the edges are directed from 
- * the first column vertex to the second column vertex). 
- * So for example, the 11th row looks liks : "2 47646". This just means that the vertex 
- * with label 2 has an outgoing edge to the vertex with label 47646
- * 
- * Your task is to code up the algorithm from the video lectures for computing strongly connected components (SCCs), 
- * and to run this algorithm on the given graph. 
- * 
- * Output Format: You should output the sizes of the 5 largest SCCs in the given graph, 
- * in decreasing order of sizes, separated by commas (avoid any spaces). 
- * So if your algorithm computes the sizes of the five largest SCCs to be 500, 400, 300, 200 and 100, 
- * then your answer should be "500,400,300,200,100". If your algorithm finds less than 5 SCCs, 
- * then write 0 for the remaining terms. Thus, if your algorithm computes only 3 SCCs whose sizes are 
- * 400, 300, and 100, then your answer should be "400,300,100,0,0".
- * 
- * WARNING: This is the most challenging programming assignment of the course. 
- * Because of the size of the graph you may have to manage memory carefully. 
- * The best way to do this depends on your programming language and environment, 
- * and we strongly suggest that you exchange tips for doing this on the discussion forums.
- */
-
 import java.io.*;
 import java.util.*;
 
@@ -30,85 +6,112 @@ public class SCC {
 	
 	final static int NUM = 875714;
 	//final static int NUM = 9;
-	//public static List<Vertex> vertices = new ArrayList<Vertex>(NUM);
 	public static Vertex[] graph = new Vertex[NUM];
 	public static Vertex[] graphRev = new Vertex[NUM];
 	
+	public int t = 0; // the number of vertices that have been fully explored
+	public Vertex leader = null; // the vertex from which the last DGS call was invoked
+	public static List<Integer> sizeList = new ArrayList<Integer>();
+	
 	public void reverseGraph(Vertex[] graph){
+		
 		for ( int i = 0; i < NUM; i++){
-			
 			Vertex vi = graph[i];
 			for(int n : vi.neighbor){
 				int fn = graph[n-1].fv; // n's fv
-				graphRev[fn-1].addEdge(vi.fv);
+				graphRev[fn-1].addEdge(vi.fv); // use vi's processing time as vertex
 			}
 		}
 	}
 	
-	public int t = 0; // the number of vertices that have been fully explored
-	public static List<Vertex> fvertices = new ArrayList<Vertex>(NUM);
+	
 	public void DFSLoop1(Vertex[] graph){
 		
 		for (int i = NUM - 1; i >= 0; i--){
-			
 			Vertex vi = graph[i];
-			//System.out.println("i: " + (i+1) + ", vi: " + vi);
 			if (vi.explored == false){
-				//s = vi;
 				DFS1(graph, vi);
 			}
 		}
 	}
 	
 	public void DFS1(Vertex[] graph, Vertex vi){
+		Stack<Vertex> stack = new Stack<Vertex>();
+		stack.push(vi);
+		
 		vi.explored = true;
 		
-		for (int n : vi.neighbor){ 
-			Vertex vj = graph[n-1];
-			//System.out.println("n: " + n + ", vj: " + vj);
-			if (vj.explored == false){
-				DFS1(graph,vj);
-			}
-		}
+		while(!stack.isEmpty()) {
+			vi = stack.peek();
 		
-		t++;
-		vi.fv = t;
-		graphRev[t-1] = new Vertex(vi.vertex, vi.fv);
-		fvertices.add(new Vertex(vi.vertex, vi.fv));
-		//System.out.println("Vertex: " + vi.vertex + ", f(v): " + vi.fv);
+			if(vi.neighbor.size() != 0){
+				
+				int n = 0;
+				while(n < vi.neighbor.size()) {
+					Vertex vj = graph[vi.neighbor.get(n) - 1];
+					if (vj.explored == false){
+						vj.explored = true;
+						stack.push(vj);
+						vi = vj;
+						n = 0;
+					}else {
+						// advance n until we find an unexplored neighbor of vi 
+						n++;
+					}
+				}
+			}
+			// Bug fixed: have to pop to end the loop
+			// was included this part inside if clause 
+			vi = stack.pop();
+			t++;
+			vi.fv = t;
+			graphRev[t-1] = new Vertex(vi.vertex, vi.fv);
+				
+		}
 	}
 	
-	
-	public Vertex s = null; // the vertex from which the last DGS call was invoked
-	public static List<Integer> sizeList = new ArrayList<Integer>();
-	
 	public void DFSLoop2(Vertex[] graphRev){
-		
 		for (int i = NUM - 1; i >= 0; i--){
 			
 			Vertex vi = graphRev[i];
-			//System.out.println("i: " + (i+1) + ", vi: " + vi);
 			if (vi.explored == false){
-				s = vi;
-				//System.out.println("Leader: " + vi.fv);
+				leader = vi;
 				ArrayList<Vertex> scc = new ArrayList<Vertex>();
 				DFS2(graphRev, vi, scc);
-				sizeList.add(scc.size());	
+				sizeList.add(scc.size());
 			}
 		}
 	}
 	
-	public void DFS2(Vertex[] graph, Vertex vi, List<Vertex> scc){
+	public void DFS2(Vertex[] graphRev, Vertex vi, List<Vertex> scc){
+		Stack<Vertex> stack = new Stack<Vertex>();
+		stack.push(vi);
+		
 		vi.explored = true;
 		scc.add(vi);
 		
-		//LinkedList<Integer> neighbor = vi.neighbor;
-		for (int n : vi.neighbor){
-			//System.out.println("n: "+ n + ", vi: " + vi);
-			Vertex vj = graphRev[n-1];
-			if (vj.explored == false){
-				DFS2(graph, vj, scc);
+		while(!stack.isEmpty()) {
+			vi = stack.peek();
+		
+			if(vi.neighbor.size() != 0){
+				
+				int n = 0;
+				while(n < vi.neighbor.size()) {
+					Vertex vj = graphRev[vi.neighbor.get(n) - 1];
+					
+					if (vj.explored == false){
+						vj.explored = true;
+						stack.push(vj);
+						scc.add(vj);
+						vi = vj;
+						n = 0;
+					}else {
+						n++;
+					}
+				}
 			}
+			vi = stack.pop();
+			vi.leader = leader.vertex;
 		}
 	}
 	
@@ -131,27 +134,14 @@ public class SCC {
 		     
 		     graph[tail-1].addEdge(head);
 		     
-//		     if (graph[tail-1] == null){
-//
-//		    	 Vertex v = new Vertex(tail);
-//		    	 v.addEdge(head);
-//		    	 graph[tail-1] = v;
-//		    	 
-//		     }else{
-//		    	 graph[tail-1].addEdge(head);
-//		     }
 		}
 		rd.close();	
-		
-//		for(int i=0; i<9; i++){
-//			System.out.println("index: " + (i+1) + ", vertex: " + graphRev[i]);
-//		}
 		
 	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		SCC2 scc = new SCC2();
+		SCC scc = new SCC();
 		//scc2.readAdjlist("TestSCC.txt");
 		scc.readAdjlist("SCC.txt");
 		
@@ -159,14 +149,14 @@ public class SCC {
 		scc.reverseGraph(graph);
 		scc.DFSLoop2(graphRev);
 		
-//		for(int i=0; i<9; i++){
-//			//System.out.println("index(fv): " + (i+1) + ", neighbor: " + graphRev[i].neighbor);
-//			System.out.println(graphRev[i]);
-//		}
-		
 		Collections.sort(sizeList);
 		for(int i = sizeList.size() - 1; i >= sizeList.size() - 5; i--){
-			System.out.println(sizeList.get(i));
+			if (sizeList.get(i) == null){
+				System.out.println("0");
+			}else{
+				System.out.println(sizeList.get(i));
+			}
+			
 		}
 	}
 
--- StanfordAlgorithm/src/Vertex.java
+++ StanfordAlgorithm/src/Vertex.java
@@ -1,20 +1,31 @@
+import java.util.*;
 
 public class Vertex {
 	int vertex;
-	int fv;
-	boolean explored;
+	int fv = -1;
+	int leader = -1;
+	boolean explored = false;
+	LinkedList<Integer> neighbor = new LinkedList<Integer>();
 	
-	public Vertex(int v, int fv, boolean explored) {
+	public Vertex(int v) {
+		this.vertex = v;
+	}
+	
+	public Vertex(int v, int fv) {
 		this.vertex = v;
 		this.fv = fv;
-		this.explored = explored;
 	}
 	
-	public Vertex(int v, boolean explored) {
-		this.vertex = v;
-		this.fv = 0;
+	public void addEdge(Integer vertex) {
+		 neighbor.add(vertex);
 	}
 	
-	public Vertex() {
+	public void removeEdge(Integer vertex) {
+		neighbor.remove(vertex);
 	}
+	
+	@Override
+    public String toString() {
+            return "leader:" + leader + ", time:" + fv + ", explored: " + explored + ", edge:" + neighbor;
+    }
 }
